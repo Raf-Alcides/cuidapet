0c514d1c47b4026cc5d3c557ff49eb4f7ab17e53
Rafael Alcides <devrafalcides@gmail.com>
2023-02-27 21:02:50 -0300

    config package Asuka

--- lib/app/app_widget.dart
+++ lib/app/app_widget.dart
@@ -1,14 +1,18 @@
+import 'package:asuka/asuka.dart' as asuka;
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
 class AppWidget extends StatelessWidget {
+  const AppWidget({Key? key}) : super(key: key);
 
-  const AppWidget({ Key? key }) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
 
-   @override
-   Widget build(BuildContext context) {
-        return MaterialApp.router(
+    Modular.setObservers([asuka.Asuka.asukaHeroController]);
+
+    return MaterialApp.router(
       title: 'Cuidapet',
+      builder: asuka.Asuka.builder,
       theme: ThemeData(primarySwatch: Colors.blue),
       routeInformationParser: Modular.routeInformationParser,
       routerDelegate: Modular.routerDelegate,
