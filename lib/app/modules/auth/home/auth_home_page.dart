f9e4d55d27bacb58c2922ffba467c9e397d7d055
Rafael Alcides <devrafalcides@gmail.com>
2023-03-06 13:56:58 -0300

    add  flutter screen utils

--- lib/app/modules/auth/home/auth_home_page.dart
+++ lib/app/modules/auth/home/auth_home_page.dart
@@ -1,3 +1,4 @@
+import 'package:cuidapet/app/core/ui/extension/size_screen_extension.dart';
 import 'package:flutter/material.dart';
 
 class AuthHomePage extends StatelessWidget {
@@ -9,8 +10,8 @@ class AuthHomePage extends StatelessWidget {
       body: Center(
         child: Image.asset(
           'assets/images/logo.png',
-          width: 162,
-          height: 130,
+          width: 162.w,
+          height: 130.h,
           fit: BoxFit.contain,
         ),
       ),
