5ad2a0353c820be3f95fb4e30920922c0bc0eeba
Rafael Alcides <devrafalcides@gmail.com>
2023-03-09 15:41:41 -0300

    udpate add ModuleRout

--- lib/app/modules/auth/auth_module.dart
+++ lib/app/modules/auth/auth_module.dart
@@ -1,3 +1,4 @@
+import 'package:cuidapet/app/modules/auth/login/login_module.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
 import 'home/auth_home_page.dart';
@@ -9,7 +10,8 @@ class AuthModule extends Module {
 
    @override
    final List<ModularRoute> routes = [
-      ChildRoute(Modular.initialRoute, child: (_, __) => const AuthHomePage())
+      ChildRoute(Modular.initialRoute, child: (_, __) => AuthHomePage(authStore: Modular.get(),)),
+      ModuleRoute('/login', module: LoginModule()) 
    ];
 
 }
